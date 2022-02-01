ac794a9c3047dfa3f21549f75283841e3abc04e3
Pubet Choompusueb <pubet50@gmail.com>
2022-02-01 17:03:31 +0700

    completed

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -2,11 +2,32 @@
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2){
+    Rect a;
+    if((R1->x+R1->w) > R2->x){
+        a.x = R2->x;
+        a.w = (R1->x+R1->w) - R2->x;
+        if(R1->x+R1->w>R2->x+R2->w){
+            a.w = R2->w;
+        }
+    }else if((R1->x+R1->w) <= R2->x){
+        a.w = 0;
+        a.x = 0;
+    }
 
-
-
+    if(R2->y > R1->y-R1->h){
+        a.y = R1->y;
+        a.h = R1->y-(R2->y-R2->h);
+        if((R1->y-R1->h)<(R2->y-R2->h) && R1->y > R2->y){
+            a.y = R2->y;
+            a.h = R2->h;
+        }
+    }else if((R1->x+R1->w) <= R2->x){
+        a.y = 0;
+        a.h = 0;
+    }
+    return a.w*a.h;
 }
